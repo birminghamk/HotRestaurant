18c91a43d7322b72be6d2f9878daa94e29dc8039
Kate Birmingham <kabi6686@colorado.edu>
2018-02-26 14:22:17 -0700

    started front-end javascript for reserve.html

--- server.js
+++ server.js
@@ -40,15 +40,15 @@ var waitlist = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/waitlist", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 
